a528b42f17c757ec55f322575e8e04dc56c0fa22
Karan <karanvknarayanan@gmail.com>
2019-09-05 19:28:24 +0530

    io

--- ioerror.py
+++ ioerror.py
@@ -128,7 +128,8 @@ class PriorityQueue:
 
 puzzle = list(range(9))
 shuffle(puzzle)
-puzzle = [1, 2, 3, 4, 5, 6, 7, 0, 8]
+puzzle = [1, 2, 3, 6, 5, 8, 7, 0, 4]
 solver = Solver(puzzle)
 solver.solve()
 print("hello")
+
